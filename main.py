35539403957cbaf2076fa2f575b3be3f831354eb
GalimullinI <G00diks@yandex.ru>
2019-11-15 19:38:09 +0300

    Интерфейс подключается к программе не в виде ui-файла, а в виде отдельного класса на языке программирования Python.

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import sys
-from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton
-from random import randint
 from PyQt5.QtGui import QPainter, QColor
+from random import randint
+from Ui import Ui_MainWindow
 
 
-class Circles(QMainWindow):
+class Circles(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('Ui.ui', self)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.run)
         self.coords = [0, 0]
         self.click = 0
-        self.setWindowTitle('Жёлтые окружности')
+        self.setWindowTitle('Случайные окружности')
 
     def run(self):
         self.click = 1
@@ -22,16 +22,16 @@ class Circles(QMainWindow):
         if self.click == 1:
             qp = QPainter()
             qp.begin(self)
-            for i in range(3):
+            for i in range(5):
                 self.drawCircle(qp)
             qp.end()
         else:
             pass
 
     def drawCircle(self, qp):
-        self.coords = [randint(100, 500), randint(100, 500)]
+        self.coords = [randint(100, 900), randint(100, 600)]
         x = randint(50, 300)
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
         qp.drawEllipse(self.coords[0] - x // 2, self.coords[1] - x // 2, x, x)
         self.click = 0
 
